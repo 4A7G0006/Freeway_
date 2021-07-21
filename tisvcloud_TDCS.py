c2e711694ae58cbfc3b07adb9d41b17b3e4d6e1c
4A7G0006 <81042814+4A7G0006@users.noreply.github.com>
2021-07-22 02:51:56 +0800

    Update tisvcloud_TDCS.py

--- tisvcloud_TDCS.py
+++ tisvcloud_TDCS.py
@@ -1,10 +1,18 @@
 import os
 import wget
 from bs4 import BeautifulSoup
+import tarfile
+import shutil
 
-
+#------------------------------------
+'''
+檔案較大下載大約需花上三天
+解壓縮的部分也順便寫好
+總共跑完大約需花上一個多星期不等--
+'''
+#------------------------------------
 def makedir_s(names):
-    path = '.\\' + names
+    path = names
     if os.path.exists(path):  # 資料夾是否存在
         pass
     else:
@@ -14,23 +22,24 @@ def makedir_s(names):
         print("[ INFO: Prepare folder create Successful... ]")
 
 
-def get_freeway_data():
+def get_freeway_data(names):
     lists = []
+    path = names + '\\'
     freeway_data = "https://tisvcloud.freeway.gov.tw/history/TDCS"
-    wget.download(freeway_data, "data.html")
-    with open("data.html", "r", encoding='utf-8-sig')as files:
+    wget.download(freeway_data, path + "data.html")
+    with open(path + "data.html", "r", encoding='utf-8-sig')as files:
         get_all = BeautifulSoup(files.read(), 'html.parser')
     table = get_all.find('table', class_='table style-1').find('tbody')
     for content in table.find_all('a')[:0:-1]:
         temp = content.text + "\n" + freeway_data + "/" + content.text + "/"
         lists.append(temp)
-    with open("maindata.txt", "a", encoding='utf-8-sig')as file:
+    with open(path + "maindata.txt", "w", encoding='utf-8-sig')as file:
         file.write("\n".join(lists))
 
 
 def download_html(path):
     html_path = path + '\\' + "total_data"
-    with open("maindata.txt", "r", encoding='utf-8-sig')as file:
+    with open(path + '\\' + "maindata.txt", "r", encoding='utf-8-sig')as file:
         list_all = file.read().splitlines()
     # print(list_all)
     for n, s_file in enumerate(list_all):
@@ -46,9 +55,9 @@ def download_html(path):
             print(html_path + '\\' + list_all[n - 1] + '.html', end="")
             try:
                 wget.download(s_file, path_download)  # 下載
-                print(" Pass")
+                print(" Successful")
             except:
-                print(" Fail")
+                print(" Failed")
                 pass
 
 
@@ -74,9 +83,9 @@ def get_file(path):
     all_gz = []
     times = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']  # 一天的小時數
     mins = ['0000', '0500', '1000', '1500', '2000', '2500', '3000', '3500', '4000', '4500', '5000', '5500']  # 每小時內的時間
-    with open("maindata.txt", "r", encoding='utf-8-sig')as file_s:
+    with open(path + '\\' + "maindata.txt", "r", encoding='utf-8-sig')as file_s:
         link_all = file_s.read().splitlines()[1::2]
-    with open("maindata.txt", "r", encoding='utf-8-sig')as file_m:
+    with open(path + '\\' + "maindata.txt", "r", encoding='utf-8-sig')as file_m:
         file_all = file_m.read().splitlines()[::2]
     waydata_path = path + '\\' + "time_list" + '\\'
     waydatas = os.listdir(waydata_path)
@@ -114,27 +123,67 @@ def get_file(path):
                     link_path1 = link_path + time_s + '/' + "TDCS_" + file_all[n] + "_" + mid + "_" + time_s + min + ".csv"
                     print("--GET " + link_path1)
                     # https: // tisvcloud.freeway.gov.tw / history / TDCS / M03A / 20210708 / 17 / TDCS_M03A_20210708_174000.csv
-                    try:
-                        wget.download(link_path1, new_path_1 + "\TDCS_" + file_all[n] + "_" + mid + "_" + time_s + min + ".csv")
-                        print("---" + "TDCS_" + file_all[n] + "_" + mid + "_" + time_s + min + ".csv" + "   Download Successful!")
-                    except:
-                        print("---" + "TDCS_" + file_all[n] + "_" + mid + "_" + time_s + min + ".csv" + "   Download Failed!")
+                    savepath = new_path_1 + "\TDCS_" + file_all[n] + "_" + mid + "_" + time_s + min + ".csv"
+                    if not os.path.exists(savepath):
+                        try:
+                            wget.download(link_path1, savepath)
+                            print("---" + "TDCS_" + file_all[n] + "_" + mid + "_" + time_s + min + ".csv" + "   Download Successful!")
+                        except:
+                            print("---" + "TDCS_" + file_all[n] + "_" + mid + "_" + time_s + min + ".csv" + "   Download Failed!")
+                            pass
+                    else:
+                        print("Pass")
                         pass
         print(".gz file start download--------------")
         for gz_files in all_gz[n]:
             link = link_all[n] + gz_files
             print(link)
             print("---" + gz_files)
-            try:
-                wget.download(link, path + '\\' + file_all[n] + '\\' + gz_files)
-                print("    Pass")
-            except:
-                print("    Failed")
+            new_save = path + '\\' + file_all[n] + '\\' + gz_files
+            if not os.path.exists(new_save):
+                try:
+                    wget.download(link, new_save)
+                    print("    Successful")
+                except:
+                    print("    Failed")
+            else:
+                print("PASS")
+                pass
 
 
-name = "Data"  # input folder name
-makedir_s(name)
-get_freeway_data()
-download_html(".\\" + name)
-catch_Serial(".\\" + name)
-get_file(".\\" + name)
+def tar_extract(file_path, name):  # 檔案解壓縮
+    tar = tarfile.open(file_path + '\\' + name, 'r:gz')
+    print("extract file -- " + name)
+    tar.extractall(path=file_path)
+
+
+def gz_file_ex(path):
+    with open(path + '\\' + "maindata.txt", "r", encoding='utf-8-sig')as file_m:
+        file_all = file_m.read().splitlines()[::2]
+    for file in file_all:
+        ex_path = path + '\\' + file
+        allfile = os.listdir(ex_path)
+        for item in allfile:
+            if item[-7:] == ".tar.gz":
+                tar_extract(ex_path, item)
+                os.remove(ex_path + '\\' + item)
+                print("-remove " + item)
+        deep_path = ex_path + '\\' + file
+        deep_file = os.listdir(deep_path)
+        for deep in deep_file:
+            if not os.path.exists(ex_path + '\\' + deep):
+                shutil.move(deep_path + '\\' + deep, ex_path + '\\' + deep)
+                print(deep + " File move exactly")
+        shutil.rmtree(deep_path)
+        print("remove folder:" + deep_path)
+
+
+if __name__ == '__main__':  # 主程式在這
+    name = "F:\\Data"  # input folder name or folder path
+    makedir_s(name)
+    get_freeway_data(name)
+    download_html(name)
+    catch_Serial(name)
+    get_file(name)
+    gz_file_ex(name)
+
